6aa44c4fe1a1d5ba5f00e065c99239d5eb9d2c79
Kai Wang <charles.wangkai@gmail.com>
2026-01-04 16:09:49 +0800

    Add Java FenwickTree

---
+++ java/FenwickTree.java
@@ -0,0 +1,24 @@
+class FenwickTree {
+  int[] a;
+
+  FenwickTree(int size) {
+    a = new int[Integer.highestOneBit(size) * 2 + 1];
+  }
+
+  void add(int pos, int delta) {
+    while (pos < a.length) {
+      a[pos] += delta;
+      pos += pos & -pos;
+    }
+  }
+
+  int computePrefixSum(int pos) {
+    int result = 0;
+    while (pos != 0) {
+      result += a[pos];
+      pos -= pos & -pos;
+    }
+
+    return result;
+  }
+}
