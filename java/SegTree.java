2120b6141b6ca798c5df63766f9a727e7318c15d
Kai Wang <charles.wangkai@gmail.com>
2026-01-04 16:37:47 +0800

    Add Java SegTree

---
+++ java/SegTree.java
@@ -0,0 +1,68 @@
+class SegTree {
+  Node root;
+
+  SegTree(int[] values) {
+    root = buildNode(values, 0, values.length - 1);
+  }
+
+  private Node buildNode(int[] values, int beginIndex, int endIndex) {
+    if (beginIndex == endIndex) {
+      return new Node(beginIndex, endIndex, values[beginIndex], null, null);
+    }
+
+    int middleIndex = (beginIndex + endIndex) / 2;
+    Node left = buildNode(values, beginIndex, middleIndex);
+    Node right = buildNode(values, middleIndex + 1, endIndex);
+
+    return new Node(beginIndex, endIndex, Math.min(left.minValue, right.minValue), left, right);
+  }
+
+  void update(int index, int value) {
+    update(index, value, root);
+  }
+
+  private void update(int index, int value, Node node) {
+    if (node.beginIndex <= index && node.endIndex >= index) {
+      if (node.beginIndex == node.endIndex) {
+        node.minValue = value;
+      } else {
+        update(index, value, node.left);
+        update(index, value, node.right);
+
+        node.minValue = Math.min(node.left.minValue, node.right.minValue);
+      }
+    }
+  }
+
+  public int query(int beginIndex, int endIndex) {
+    return query(beginIndex, endIndex, root);
+  }
+
+  private int query(int beginIndex, int endIndex, Node node) {
+    if (node.beginIndex > endIndex || node.endIndex < beginIndex) {
+      return Integer.MAX_VALUE;
+    }
+    if (node.beginIndex >= beginIndex && node.endIndex <= endIndex) {
+      return node.minValue;
+    }
+
+    return Math.min(
+        query(beginIndex, endIndex, node.left), query(beginIndex, endIndex, node.right));
+  }
+
+  static class Node {
+    int beginIndex;
+    int endIndex;
+    int minValue;
+    Node left;
+    Node right;
+
+    Node(int beginIndex, int endIndex, int minValue, Node left, Node right) {
+      this.beginIndex = beginIndex;
+      this.endIndex = endIndex;
+      this.minValue = minValue;
+      this.left = left;
+      this.right = right;
+    }
+  }
+}
