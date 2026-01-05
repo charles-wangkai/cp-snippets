6539e5292b514de4adacd55385f502b74cfac6ac
Kai Wang <charles.wangkai@gmail.com>
2026-01-05 17:07:28 +0800

    Add Java MaxFlow

---
+++ java/MaxFlow.java
@@ -0,0 +1,112 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+
+class MaxFlow {
+  List<Edge> edges = new ArrayList<>();
+  List<Integer>[] edgeLists;
+
+  @SuppressWarnings("unchecked")
+  MaxFlow(int size) {
+    edgeLists = new List[size];
+    for (int i = 0; i < edgeLists.length; ++i) {
+      edgeLists[i] = new ArrayList<>();
+    }
+  }
+
+  void addEdges(int u, int v, int cap) {
+    edges.add(new Edge(u, v, cap));
+    edgeLists[u].add(edges.size() - 1);
+
+    edges.add(new Edge(v, u, 0));
+    edgeLists[v].add(edges.size() - 1);
+  }
+
+  int dinic(int s, int t) {
+    int result = 0;
+    while (true) {
+      int[] levels = bfs(s, t);
+      if (levels == null) {
+        break;
+      }
+
+      while (true) {
+        int minflow = dfs(levels, s, t, Integer.MAX_VALUE);
+        if (minflow == 0) {
+          break;
+        }
+
+        result += minflow;
+      }
+    }
+
+    return result;
+  }
+
+  private int[] bfs(int s, int t) {
+    int[] levels = new int[edgeLists.length];
+    Arrays.fill(levels, -1);
+    levels[s] = 0;
+
+    Queue<Integer> queue = new ArrayDeque<>();
+    queue.offer(s);
+
+    while (!queue.isEmpty()) {
+      int head = queue.poll();
+      if (head == t) {
+        return levels;
+      }
+
+      for (int e : edgeLists[head]) {
+        Edge edge = edges.get(e);
+        if (edge.capacity != 0 && levels[edge.to] == -1) {
+          levels[edge.to] = levels[head] + 1;
+          queue.offer(edge.to);
+        }
+      }
+    }
+
+    return null;
+  }
+
+  private int dfs(int[] levels, int s, int t, int low) {
+    if (s == t) {
+      return low;
+    }
+
+    int result = 0;
+    for (int e : edgeLists[s]) {
+      Edge edge = edges.get(e);
+      if (edge.capacity != 0 && levels[edge.to] == levels[s] + 1) {
+        int next = dfs(levels, edge.to, t, Math.min(low - result, edge.capacity));
+        edge.capacity -= next;
+        edges.get(e ^ 1).capacity += next;
+
+        result += next;
+        if (result == low) {
+          break;
+        }
+      }
+    }
+
+    if (result == 0) {
+      levels[s] = -1;
+    }
+
+    return result;
+  }
+
+  static class Edge {
+    int from;
+    int to;
+    int capacity;
+
+    Edge(int from, int to, int capacity) {
+      this.from = from;
+      this.to = to;
+      this.capacity = capacity;
+    }
+  }
+}
